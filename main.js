a37047b0660d3a71c5c16c65d2e4be1ae1a8ce56
chirag-parmar16 <151434796+chirag-parmar16@users.noreply.github.com>
2026-05-05 10:33:55 +0530

    Setup code

---
+++ main.js
@@ -0,0 +1,9 @@
+// JavaScript to change navbar color on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
